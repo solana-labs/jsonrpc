f28a82a38b6bb778697ae8fad14180869e73f613
Tyera Eulberg <tyera@solana.com>
2018-09-27 14:15:32 -0600

    Automate publish to crates.io

---
+++ ci/publish-crates.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+subdirs=(core server-utils http tcp pubsub macros)
+CRATES_IO_TOKEN=${1}
+
+for i in ${subdirs[@]}; do
+  cd ${i}
+  cargo package
+  cargo publish --token "$CRATES_IO_TOKEN"
+  cd ..
+done
+
+exit 0
